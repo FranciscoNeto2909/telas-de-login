0d00b246d447d44934d44d1697625b62bcfbd4d0
Francisco Neto <wantedshark@gmail.com>
2023-05-19 15:52:03 -0300

    home and pages improved

--- instinct/script.js
+++ instinct/script.js
@@ -5,25 +5,25 @@ const pass = document.querySelector('#pass')
 const btn = document.querySelector('#button')
 btn.addEventListener("click", () => {
     if (name.value === "") {
-        name.style.border = "1px solid red"
-        setTimeout(() => name.style.border = "", 2000);
+        name.style.borderBottom = "1px solid red"
+        setTimeout(() => name.style.borderBottom = "", 2000);
     } else if (email.value === "") {
-        email.style.border = "1px solid red"
-        setTimeout(() => email.style.border = "", 2000);
+        email.style.borderBottom = "1px solid red"
+        setTimeout(() => email.style.borderBottom = "", 2000);
     } else if (pass.value === "") {
-        pass.style.border = "1px solid red"
-        setTimeout(() => pass.style.border = "", 2000);
+        pass.style.borderBottom = "1px solid red"
+        setTimeout(() => pass.style.borderBottom = "", 2000);
     }
 
     else if (email.value.indexOf("@") === -1 || email.value.indexOf(".") === -1
         || (email.value.indexOf("@") - email.value.indexOf(".") !== -6)) {
-        email.style.border = "1px solid red"
+        email.style.borderBottom = "1px solid red"
         alert("O Email deve ter o nome do usuario seguido de @gmail.com")
-        setTimeout(() => email.style.border = "", 2000);
+        setTimeout(() => email.style.borderBottom = "", 2000);
     } else if (pass.value.length < 4) {
         alert("A senha deve ter no minimo 4 digitos")
-        pass.style.border = "1px solid red"
-        setTimeout(() => email.style.border = "", 2000);
+        pass.style.borderBottom = "1px solid red"
+        setTimeout(() => email.style.borderBottom = "", 2000);
     } else {
         document.querySelector(".wellcome").style.display = "block"
         setTimeout(() => {
